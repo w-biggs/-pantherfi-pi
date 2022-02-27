513d6742ea4bb61407b9343908c1fd590b733849
= <=>
2022-02-26 22:35:20 -0500

    added passkey and updated readme

--- cleandata.py
+++ cleandata.py
@@ -12,6 +12,7 @@ def load(filename):
 			"upload": data['upload']/1000000,
 			"ping":  data['ping'],
 			"hostname": socket.gethostname(),
+			"passkey": "ylireagfiurhriulaour;oi45385049ufefj09wfr9not098ej09tegh9--e9jfj-g80q3hjq9--r9fj4tvv",
 		}
 		
 	except:
@@ -20,6 +21,7 @@ def load(filename):
 			"upload": 0,
 			"ping":  0,
 			"hostname": socket.gethostname(),
+			"passkey": "ylireagfiurhriulaour;oi45385049ufefj09wfr9not098ej09tegh9--e9jfj-g80q3hjq9--r9fj4tvv",
 		}
 	finally:
 		return cleanData
